eeb41d852ad63ea471443e230964b34eb88f9826
Raluca Curt <ralucaioanacurt@gmail.com>
2023-02-20 17:51:54 +0200

    added password hashing to user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework.serializers import ModelSerializer
 from .models import *
 
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())],
+            min_length=5,
+            max_length=20
+            ),
+    password = serializers.CharField(
+            required=True,
+            max_length=256
+            )
     class Meta:
         model = User
         fields = '__all__'
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
         
 class ItemSerializer(ModelSerializer):
     class Meta:
